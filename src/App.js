d35685d00d786c9d73f8473cb309278325653bd3
Pollard <624730@bah.com>
2024-01-08 09:03:16 -0500

    initial commit for skeleton react app to transmit and receive from a spring web service, ultimately to kafka.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+async function hitServer(){
+  const response = await fetch('http://localhost:8080/greeting?name=Jim');
+  const greet = await response.json();
+  return greet;
+}
 
+const getGreeting = async () => {
+  const data = await hitServer();
+  console.log((data));
+  document.getElementById('greeting').innerHTML = data.content;
+  
+}
+
+const postMessage = async () => {
+  const subject = document.getElementById('subject').value;
+  const body = document.getElementById('content').value;
+  const response = await fetch('http://localhost:8080/postit', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      subject,
+      body
+    })
+  });
+  alert("successfully posted");
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="btn btn-primary" onClick={()=>getGreeting()}>Get Greeting</button>
+        <label id="greeting"></label>
+        <br/>
+        <input id="subject" type="text" placeholder="Enter a Subject" /><br/>
+        <input id="content" type="text" placeholder="Enter some content" /><br/>
+        <button className="btn btn-primary" onClick={()=> postMessage()}>Post</button>
       </header>
     </div>
   );
